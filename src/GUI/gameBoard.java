3ceddbaa3c12a9c349a8179326e4c8656b4c607e
moshe <moshe@DESKTOP-8FQQRGC>
2018-12-17 22:39:30 +0200

    change in gui now the packman move and add to kml

--- src/GUI/gameBoard.java
+++ src/GUI/gameBoard.java
@@ -1,27 +1,20 @@
 package GUI;
 
-import java.awt.Color;
-import java.awt.Graphics;
+
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
 import Fileformat.CsvData;
-import Fileformat.CsvReader;
+
 
 public class gameBoard extends JFrame //implements MouseListener
 {
@@ -106,10 +99,11 @@ public class gameBoard extends JFrame //implements MouseListener
 					fileChooser.setAcceptAllFileFilterUsed(false);
 					FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV file", "csv");
 					fileChooser.setFileFilter(filter);
-					fileChooser.showOpenDialog(null);
+					if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
 					file = fileChooser.getSelectedFile();
 					data= new CsvData(file);
 					ImageBackground.setGame(data);
+					}
 			
 			}
 			
@@ -131,12 +125,13 @@ public class gameBoard extends JFrame //implements MouseListener
 				fileChooser.setAcceptAllFileFilterUsed(false);
 				FileNameExtensionFilter filter = new FileNameExtensionFilter("KML file", "kml");
 				fileChooser.setFileFilter(filter);
-				fileChooser.showSaveDialog(null);
+				if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
 				File file2=fileChooser.getSelectedFile();
 				String saveFile=file2.getPath()+".kml";
 				ImageBackground.setSaveTo(saveFile);
 				ImageBackground.saveToKML();
 				System.out.println(saveFile);
+				}
 			}
 			
 		}
